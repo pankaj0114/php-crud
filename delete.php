328b063400ced9d216b7da0a06038e8936ad5a5d
Arcs Infotech <navepala@gmail.com>
2023-06-02 16:26:40 +0530

    done 2nd june

--- delete.php
+++ delete.php
@@ -33,7 +33,7 @@ if (isset($_GET['id'])) {
     </head>
     <body style="background-color:lightyellow ; text-align:center ">
         <b><h1 style="text-align:center; color: #008080; ">Employee Record deleted Successfully.</h1></b>
-        <img class ="img" src="https://img.icons8.com/?size=512&id=23446&format=png" style=" padding-left:10%" alt="logo">
+        <img class ="img" src="https://img.icons8.com/?size=512&id=23446&format=png" style=" padding-left:10% " alt="logo">
         <p><h2 style="color: crimson"> Thanks for visiting Dashboard !!</h2></p>
         <p><h2 style="color: crimson"><a href="index.php">Back to home Page</a></h2></p>
 
